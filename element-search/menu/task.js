6128193ef2e04a9189f7c9875e7484d39a9fbb65
Az-of-Tul <a.tulaev@swissin.ru>
2023-08-19 05:43:33 +0300

    Задание готово

---
+++ element-search/menu/task.js
@@ -0,0 +1,11 @@
+const menuLink = document.querySelectorAll('.menu__link');
+
+for (let link of menuLink) {
+    link.onclick = function (e) {
+        const menuSub = link.parentElement.querySelector('.menu_sub');
+        if (menuSub) {
+            e.preventDefault();
+            menuSub.classList.toggle('menu_active')
+        }
+    }
+}
