2afa11be6ed4e82ed01519241b65ebb6d11fe0ba
Az-of-Tul <a.tulaev@swissin.ru>
2023-08-25 11:12:54 +0300

    Задание готово

---
+++ async-requests/preloader/task.js
@@ -0,0 +1,16 @@
+const item = document.getElementById('items');
+const img = document.querySelector('.loader');
+const url = 'https://students.netoservices.ru/nestjs-backend/slow-get-courses';
+
+  fetch(url)
+    .then((data) => data.text())
+    .then(function (data) {
+        img.classList.remove('loader_active')
+        let valutes = JSON.parse(data).response.Valute
+        for (let key in valutes) {
+            item.innerHTML += `<div class="item">
+            <div class="item__code">${valutes[key].CharCode}
+            </div><div class="item__value">${valutes[key].Value}
+            </div><div class="item__currency">руб.</div>`
+        }
+  })
