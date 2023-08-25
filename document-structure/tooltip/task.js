4b15863f0c989ede1f9864a3257653e3301513bc
Az-of-Tul <a.tulaev@swissin.ru>
2023-08-25 10:15:45 +0300

    Задание готово

---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,22 @@
+const btn = Array.from(document.querySelectorAll('.has-tooltip'));
+const tooltipActive = document.createElement('div');
+
+tooltipActive.className = 'tooltip';
+
+btn.forEach((e) => {
+    e.addEventListener('click', (el) => {
+        el.preventDefault();
+        target = el.target;
+
+        if (target.title === tooltipActive.innerText) {
+            tooltipActive.classList.toggle('tooltip_active');
+            return;
+        } 
+
+        tooltipActive.innerText = target.title;
+        const { bottom, left } = target.getBoundingClientRect();
+        tooltipActive.style = `left: ${left}px; top: ${bottom}px`;
+        target.insertAdjacentElement('afterEnd', tooltipActive);
+        tooltipActive.classList.add('tooltip_active');
+    })
+})
